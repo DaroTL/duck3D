c50e2cc7903bd9ad762ae0b83f2bf5f8f406d1aa
Jakub <bciejor@gmail.com>
2021-10-27 22:39:29 +0200

    fgjfgjgfjgf

---
+++ w451k ch07/Vector3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Vector3
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector3(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double getX()
+        {
+            return x;
+        }
+        
+        public void setX(double value)
+        {
+            x = value;
+        }
+
+        public double getY()
+        {
+            return y;
+        }
+
+        public void setY(double value)
+        {
+            y = value;
+        }
+
+        public double getZ()
+        {
+            return z;
+        }
+
+        public void setZ(double value)
+        {
+            z = value;
+        }
+    }
+}
