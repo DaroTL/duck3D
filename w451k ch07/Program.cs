b1fb6518d1bb34f80eb8698f0b572f9e6de5407e
DaroTL <darrenkappa2532@gmail.com>
2021-10-28 15:17:05 +0200

    pen

--- w451k ch07/Program.cs
+++ w451k ch07/Program.cs
@@ -10,11 +10,77 @@ namespace w451k_ch07
     {
         static void Main(string[] args)
         {
-            Renderer x = new Renderer();
+            Renderer zzzz = new Renderer();
 
-            x.plotLine(20, 20, 10, 20);
-            x.render();
+            //             x.plotLine(20, 20, 10, 20);
+            zzzz.render();
+//             for(; ; )
+//             {
+//                 String command = Console.ReadLine();
+//                 switch (command)
+//                 {
+// 
+//                 }
+//             }
+            int[] pen = usePen();
 
+
+
+
+        }
+
+        static int[] usePen()
+        {
+            for (; ; )
+            {
+                ConsoleKeyInfo pen = Console.ReadKey();
+
+                int y = Console.CursorTop, x = Console.CursorLeft;
+                if (pen.Key == ConsoleKey.F1) return new int[] { x, y };
+                
+                if (pen.Key == ConsoleKey.DownArrow)
+                {
+                    if (y >= (y + 1))
+                        Console.SetCursorPosition(x, y - 1 + 1);
+                    else
+                        Console.SetCursorPosition(x - 1, y + 1);
+
+                }
+
+                if (pen.Key == ConsoleKey.UpArrow)
+                {
+                    if (y <= 0)
+                        Console.SetCursorPosition(x, y + 1 - 1);
+                    else
+                        Console.SetCursorPosition(x - 1, y - 1);
+
+                }
+
+                if (pen.Key == ConsoleKey.RightArrow)
+                {
+                    if (x >= (x + 1))
+                        Console.SetCursorPosition(x - 2 + 1, y);
+                    else
+                        Console.SetCursorPosition(x + 1, y);
+
+                }
+
+                if (pen.Key == ConsoleKey.LeftArrow)
+                {
+                    if (x <= 0)
+                        Console.SetCursorPosition(x + 1 - 1, y);
+                    else
+                        Console.SetCursorPosition(x - 2, y);
+
+                }
+
+                if (x > Console.WindowWidth) Console.SetCursorPosition(x, (y + 1));
+                if (pen.Modifiers == ConsoleModifiers.Alt && pen.Key == ConsoleKey.F4) Environment.Exit(0);
+                if (pen.Key == ConsoleKey.Escape) break;
+
+                 
+            }
+            return new int[]  { 0, 0 };
         }
     }
 }
